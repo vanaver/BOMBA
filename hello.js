ef11bfc05418ac2cfb91a3135fc7778f4fdf3979
Ivan <shiro08012005@gmail.com>
2024-12-02 17:23:59 +0300

    REpeayt

--- hello.js
+++
@@ -1,7 +0,0 @@
-let zvanie = document.getElementById("myName");
-let stylish = getComputedStyle(zvanie).width
-zvanie.onclick = function () {
-    this.innerHTML = "You too!"
-    this.style.width = stylish
-}
-console.log(stylish)
