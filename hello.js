d7ac9e2ffa412435709506b25407970edea13a9c
Ivan <shiro08012005@gmail.com>
2024-12-04 17:50:07 +0300

    timer

--- hello.js
+++ hello.js
@@ -15,6 +15,16 @@ Array.from(bobas).forEach((element, index) => {
 });
 
 
+function updateTime() {
+    let span = document.getElementById('fuckdata');
+    const startDate = new Date(2024, 11, 4);
+    let datachas = new Date
+    let diff = Math.floor((datachas - startDate) / 1000)
+    span.innerHTML = diff + " "
+}
+
+setInterval(updateTime, 1000);
+updateTime()
 
 
 
